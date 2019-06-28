edd6ccc91297b74907cc398e22f2ff964565f450
ryzwannj <ryzwannj@gmail.com>
2019-06-28 13:37:31 +0200

    commit.2

--- Login.py
+++ Login.py
@@ -1,1 +1,16 @@
 import sqlite3
+
+def register():
+    name = str(input("What's your name? "))
+    username = input("Enter a username: ")
+    password = input("Enter a password: ")
+    verfpassword = input("Enter your password again: ")
+
+    while password != verfpassword:
+        register()
+        break
+
+def login():
+    username1 = input("username: ")
+    password1 = input("password: ")
+
